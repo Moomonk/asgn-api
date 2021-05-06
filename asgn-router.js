28f40af74ac0328b58b8ea78b84fcbaed09f8b59
Cody Blakert <81455139+Moomonk@users.noreply.github.com>
2021-05-06 05:37:02 -0500

    Added comments.

--- asgn-router.js
+++ asgn-router.js
@@ -1,10 +1,30 @@
-let router = require("express").Router();
+/**
+ * asgn-router.js
+ * Router file for asgn-api application
+ * Project 3
+ * Name: Cody Blakert
+ * COMP2150 Web Services
+ */
 
+let router = require("express").Router();
+let controller = require("./asgn-controller.js");
+
+// Begining page/validation it is running
 router.get("/", function (req, res) {
     res.json ({
         status: "API is working.",
         message: "Welcome to the Asgn API"
-    })
-})
+    });
+});
+
+// Linked controller methods
+router.route("/assignments")
+    .get(controller.index)
+    .post(controller.new);
+
+router.route("/assignments/:assignment_id")
+    .get(controller.view)
+    .put(controller.update)
+    .delete(controller.delete);
 
 module.exports = router;
